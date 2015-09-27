3dfd921dfa415be14ddc0f54b13e81c01b16f1b1
kurtcarpenter <kurtcarpenter@hotmail.com>
2015-09-26 23:22:46 -0400

    commit

--- background.js
+++ background.js
@@ -1,5 +1,24 @@
-chrome.history.search({text: '', maxResults: 10}, function(data) {
-    data.forEach(function(page) {
-        console.log(page.url);
-    });
-});
+chrome.extension.onMessage.addListener(
+  function (request, sender) {
+    var url = sender.tab.url
+    if (needsCulling(url)) {
+      chrome.history.search({text: url, maxResults: 1}, function(data) {
+        var item = data.pop().url
+        chrome.history.deleteUrl({"url": item}, function() {
+        })
+      })
+    }
+})
+
+var needsCulling = function (url) {
+  console.log("url " + url)
+  var cullMe = ["reddit.com", "imgur.com"]
+  for (var i = 0; i < cullMe.length; i++) {
+    if (url.indexOf(cullMe[i]) >= 0) {
+      console.log("cull " + url)
+      return true
+    }
+  }
+  console.log("gucci " + url)
+  return false
+}
